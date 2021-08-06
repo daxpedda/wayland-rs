2b5fb0bbe18d7f485f6d240b4281f00611246b10
vberger <vberger@users.noreply.github.com>
2021-08-06 18:30:20 +0000

    deploy: 82c951147d8fb13a749c5942ab5baf636f23c3ba

---
+++ implementors/std/os/imp/unix/io/trait.AsRawFd.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["wayland_commons"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.54.0/std/os/imp/unix/io/trait.AsRawFd.html\" title=\"trait std::os::imp::unix::io::AsRawFd\">AsRawFd</a> for <a class=\"struct\" href=\"wayland_commons/socket/struct.Socket.html\" title=\"struct wayland_commons::socket::Socket\">Socket</a>","synthetic":false,"types":["wayland_commons::socket::Socket"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
